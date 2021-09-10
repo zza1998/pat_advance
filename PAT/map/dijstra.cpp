a17a09473294f1728d52f1ed8e8f91f26f6fac36
zhouzian <zza1998@foxmail.com>
2021-09-10 16:28:15 +0800

    晒素数

--- PAT/map/dijstra.cpp
+++ PAT/map/dijstra.cpp
@@ -1,65 +1,78 @@
 #include "iostream"
 #include "vector"
 #include "queue"
-#include "vector"
+
 using namespace std;
-int n , m;
-int edges[100][100];
+int n, m;
+struct node;
 int visit[100];
-vector<int> nabor[100];
-struct node{
+struct node {
     int val;
     int dis;
-    node(int val,int dis){
+    node(int val, int dis) {
         this->val = val;
         this->dis = dis;
     }
-    bool operator ()  (node *& n1, node *&n2){
-        if(n1->dis!=n2->dis){
-            return n1->dis<n2->dis;
+    bool operator < (const node &n2) const {
+        if (dis != n2.dis) {
+            return dis > n2.dis;
         }
-        return n1->val<n2->val;
+        return val> n2.val;
     }
 };
-priority_queue<node,vector<node>> q;
+vector<node> nabor[100];
+priority_queue<node, vector<node>> q;
 int ori = 1;
-void dijkstra(int curr){
-    int min = 0x7ffffff;
-    int minI = -1;
-    vector<int> next = nabor[curr];
-    for (int i = 0; i < n; ++i) {
-        if(visit[i]== 0 && edges[curr][i] < min){
-            minI = i;
-            min = edges[curr][i];
-        }
-    }
-    if(minI == -1) return;
-    visit[minI] = 1;
-    for (int i = 0; i < n; ++i) {
-        if(visit[i] == 0 && edges[curr][minI] + edges[minI][i] < edges[curr][i]){
-            edges[curr][i] = edges[curr][minI] + edges[minI][i];
+int dis[100];
+void dijkstra() {
+//    int min = 0x7ffffff;
+//    int minI = -1;
+//    vector<int> next = nabor[curr];
+//    for (int i = 0; i < n; ++i) {
+//        if (visit[i] == 0 && edges[curr][i] < min) {
+//            minI = i;
+//            min = edges[curr][i];
+//        }
+//    }
+//    if (minI == -1) return;
+//    visit[minI] = 1;
+//    for (int i = 0; i < n; ++i) {
+//        if (visit[i] == 0 && edges[curr][minI] + edges[minI][i] < edges[curr][i]) {
+//            edges[curr][i] = edges[curr][minI] + edges[minI][i];
+//        }
+//    }
+
+}
+void dijkstraV2(){
+    fill(dis,dis+100,0x777777);
+    dis[ori] = 0;
+    while (!q.empty()){
+        node nd = q.top();
+        q.pop();
+        int nk = nd.val,nx = nd.dis;
+        if(visit[nk]) continue;
+        visit[nk] = 1;
+        vector<node> v = nabor[nk];
+        for (node nod : v) {
+            if(!visit[nod.val] && nod.dis+nx<dis[nod.val]){
+                dis[nod.val] = nod.dis+dis[nk];
+                q.push(node(nod.val,dis[nod.val]));
+            }
         }
     }
 }
-int main(){
-    cin>>n>>m;
-    fill(edges[0],edges[0]+100*100,0x7ffffff);
+
+int main() {
+    cin >> n >> m;
     for (int i = 0; i < m; ++i) {
-        int from,to,weight;
-        cin>>from>>to>>weight;
-        edges[from][to] = edges[to][from] = weight;
-        nabor[from].push_back(to);
-        nabor[to].push_back(from);
+        int from, to, weight;
+        cin >> from >> to >> weight;
+        nabor[from].emplace_back(to,weight);
+        nabor[to].emplace_back(from,weight);
     }
+    q.emplace(1, 0);
+    dijkstraV2();
     for (int i = 2; i <= n; ++i) {
-        q.emplace(i,edges[ori][i]);
-    }
-    for (int i = 1; i <= n; ++i) {
-        if(visit[i]==0){
-            dijkstra(ori);
-        }
-    }
-    for (int i = 2; i <= n; ++i) {
-        printf("%d to %d dis:%d\n",ori,i,edges[ori][i]);
+        printf("%d to %d dis:%d\n", ori, i, dis[i]);
     }
 }
